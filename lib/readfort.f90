031ad3ca8f87327b3d23188b7926de7e6977828b
pacargile <pcargile@cfa.harvard.edu>
2023-10-13 10:11:44 -0400

    initial code for runprep, needs testing

--- lib/readfort.f90
+++ lib/readfort.f90
@@ -350,7 +350,6 @@ module readfort
         I = 1
         DO WHILE (1.eq.1)
           READ(1,end=100)LINDAT8,LINDAT4
-            IF(I.LE.310.AND.I.GE.290) WRITE(6,*)I,WL,CODE,E
             WLi(I) = WL
             Ei(I) = E
             EPi(I) = EP
