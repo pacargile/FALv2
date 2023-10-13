b7a2258ce51335d373c863ec3b4050b624668067
pacargile <pcargile@cfa.harvard.edu>
2023-10-13 16:48:26 -0400

    initial code for runprep, needs testing

--- fal/prep/runprep.py
+++ fal/prep/runprep.py
@@ -291,7 +291,7 @@ class RunPrep(object):
                     mLL_i = mLL[f'{int(pp)}']
                     mat = True
                     for kk in sLL.keys():
-                        if kk in ['index','linesrc','ref','auto','ixfixfp','labelp','label']:
+                        if kk in ['index','linesrc','ref','auto','ixfixfp','labelp','label','ishift','ishiftp']:
                             continue
                         mat *= sLL[kk][ii] == mLL_i[kk]
                     if mat:
