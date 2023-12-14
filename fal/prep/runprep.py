e398caec8bc572ecd7cd7e1746ce2da702439701
pacargile <pcargile@cfa.harvard.edu>
2023-12-14 09:45:08 -0500

    code updates

--- fal/prep/runprep.py
+++ fal/prep/runprep.py
@@ -471,16 +471,25 @@ class RunPrep(object):
                 # y = np.array([wl_full,loggf_full,gammar_full,gammas_full,gammaw_full])
                 # nonrepeatind = np.nonzero(np.all(~np.isin(x,y).T,axis=1))[0]
 
+                # nonrepeatind = [True for _ in range(len(code_fl))]
+                # for ii,(x1,x2,x3,x4,x5) in enumerate(zip(wl_fl,loggf_fl,gammar_fl,gammas_fl,gammaw_fl)):
+                #     if (
+                #         (x1 in wl_full) and 
+                #         (x2 in loggf_full) and 
+                #         (x3 in gammar_full) and 
+                #         (x4 in gammas_full) and 
+                #         (x5 in gammaw_full)):
+                #         nonrepeatind[ii] = False
+                # nonrepeatind = np.array(nonrepeatind,dtype=bool)
+
                 nonrepeatind = [True for _ in range(len(code_fl))]
+                y = np.vstack([wl_full,loggf_full,gammar_full,gammas_full,gammaw_full]).T
                 for ii,(x1,x2,x3,x4,x5) in enumerate(zip(wl_fl,loggf_fl,gammar_fl,gammas_fl,gammaw_fl)):
-                    if (
-                        (x1 in wl_full) and 
-                        (x2 in loggf_full) and 
-                        (x3 in gammar_full) and 
-                        (x4 in gammas_full) and 
-                        (x5 in gammaw_full)):
+                    x = [x1,x2,x3,x4,x5]
+                    if len((y == x).all(axis=1).nonzero()[0]) > 0:
                         nonrepeatind[ii] = False
                 nonrepeatind = np.array(nonrepeatind,dtype=bool)
+                
 
                 print(f'     ... Adding {nonrepeatind.sum()}/{len(nonrepeatind)} to significant line list.',flush=True)
 
