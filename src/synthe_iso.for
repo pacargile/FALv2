e4c0495555ae1e4bdfb57100afdc5e92ec520fe9
pacargile <pcargile@cfa.harvard.edu>
2024-01-08 16:00:09 -0500

    code updates

--- src/synthe_iso.for
+++ src/synthe_iso.for
@@ -402,16 +402,16 @@ C
 C           DO THE CORRECTION
                   FREQ=2.99792458D17/WL
                   GF = CONGF*FREQ*1.77245D0/.026538D0
-                  GFLOGI = LOG10(GF)
+                  GFLOGI = DLOG10(GF)
             
                   IF(ISO1.EQ.ISOFRACISO1)THEN
-                  GFLOGI = GFLOGI+LOG10(1.0+(1.0/ISOFRACSOL))
-     1            -LOG10(1.0+(1.0/ISOFRACSTAR))
+                  GFLOGI = GFLOGI+DLOG10(1.0+(1.0/ISOFRACSOL))
+     1            -DLOG10(1.0+(1.0/ISOFRACSTAR))
 C                 print *, LOG10(1.0+(1.0/ISOFRACSTAR))
                   ENDIF
                   IF(ISO1.EQ.ISOFRACISO2)THEN
-                  GFLOGI = GFLOGI+LOG10(1.0+ISOFRACSOL)
-     1            -LOG10(1.0+ISOFRACSTAR)
+                  GFLOGI = GFLOGI+DLOG10(1.0+ISOFRACSOL)
+     1            -DLOG10(1.0+ISOFRACSTAR)
 C                 print *, LOG10(1.0+ISOFRACSTAR)
                   ENDIF
 
