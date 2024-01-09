5c2a91bdfe0cdebff47ac56279ba7568e4d6b37e
pacargile <pcargile@cfa.harvard.edu>
2024-01-09 11:18:08 -0500

    code updates

--- fal/prep/runprep.py
+++ fal/prep/runprep.py
@@ -440,8 +440,10 @@ class RunPrep(object):
                     # Must reverse the correction applied with synthe
                     for kk in self.specinfo[ii]['isofrac'].keys():
                         iso1,iso2,isofrac_sun,isofrac_star = self.specinfo[ii]['isofrac'][kk]
-                        corr1 = np.float32(np.log10(1.0 + (1.0/isofrac_sun)) - np.log10(1.0 + (1.0/isofrac_star)))
-                        corr2 = np.float32(np.log10(1.0 + isofrac_sun) - np.log10(1.0 + isofrac_star))
+                        corr1 = np.log10(1.0 + (1.0/isofrac_sun)) - np.log10(1.0 + (1.0/isofrac_star))
+                        corr2 = np.log10(1.0 + isofrac_sun) - np.log10(1.0 + isofrac_star)
+                        print(f'PY CORR1: {corr1}',flush=True)
+                        print(f'PY CORR2: {corr2}',flush=True)
 
                         # correct the atoms
                         cond1_atm = (synout_i['code'] < 100.0) & (synout_i['iso1'] == iso1)
@@ -499,8 +501,6 @@ class RunPrep(object):
 
                         for kk in ['wl','code','loggf','gammar','gammas','gammaw','nelion','e','ep','nblo','nbup','iso1','iso2','x1','x2','xj','xjp']:
                             print(kk,'\n',synout_i[kk][cond1_mol_1],flush=True)
-                        print(f'CORR1: {corr1}',flush=True)
-                        print(f'CORR2: {corr2}',flush=True)
 
 
                 # # write spectrum to seg_num/data/
