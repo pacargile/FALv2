7a9fcf314cb00549543347fe343ca3b0bd4bd70d
pacargile <pcargile@cfa.harvard.edu>
2024-01-09 10:23:47 -0500

    code updates

--- fal/prep/runprep.py
+++ fal/prep/runprep.py
@@ -498,7 +498,7 @@ class RunPrep(object):
                         # synout_i['loggf'] = np.array([float(f'{x:.3f}') for x in synout_i['loggf']],dtype=float)
 
                         for kk in ['wl','code','loggf','gammar','gammas','gammaw','nelion','e','ep','nblo','nbup','iso1','iso2','x1','x2','xj','xjp']:
-                            print(kk,'\n',synout_i[kk][cond1_mol_2],flush=True)
+                            print(kk,'\n',synout_i[kk][cond1_mol_1],flush=True)
                         print(f'CORR1: {corr1}',flush=True)
                         print(f'CORR2: {corr2}',flush=True)
 
