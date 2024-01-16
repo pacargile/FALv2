847f00de489314272771948adeab00d269a88720
pacargile <pcargile@cfa.harvard.edu>
2024-01-16 15:10:44 -0500

    code updates

--- lib/readfort.f90
+++ lib/readfort.f90
@@ -351,8 +351,6 @@ module readfort
         DO WHILE (1.eq.1)
           READ(1,end=100)LINDAT8,LINDAT4
 
-            WRITE(6,*) 'A', LINDAT8,LINDAT4
-
             WLi(I) = WL
             Ei(I) = E
             EPi(I) = EP
@@ -397,6 +395,10 @@ module readfort
             READ(COTHER1,'(2I5)')ISHIFT,ISHIFTP
             READ(COTHER2,'(A6,I1,A3)')IXFIXFP,LINESIZE,AUTO
 
+            IF(WL.GT.517.449.AND.WL.GT.517.450)THEN
+                WRITE(6,*)'A',LINDAT8,LINDAT4
+            ENDIF
+
             ! WRITE(6,'(F10.4,I5,I5,A6,I5,A3)') WL,ISHIFT,ISHIFTP,IXFIXFP,LINESIZE,AUTO
 
             ISHIFTi(I) = ISHIFT
@@ -428,7 +430,13 @@ module readfort
             ! END DO
             OTHER1x(1,I) = OTHER1(1)
             OTHER1x(2,I) = OTHER1(2)
+
             
+            IF(WL.GT.517.449.AND.WL.GT.517.450)THEN
+                WRITE(6,*)'B',LINDAT8,LINDAT4
+            ENDIF
+
+
             ! WRITE(SLABEL,'(A7)') OTHER2(1)
             ! SLABEL = SLABEL//c_null_char
             ! K = 1
@@ -451,6 +459,10 @@ module readfort
             OTHER2x(1,I) = OTHER2(1)
             OTHER2x(2,I) = OTHER2(2)
 
+            IF(WL.GT.517.449.AND.WL.GT.517.450)THEN
+                WRITE(6,*)'C',LINDAT8,LINDAT4
+            ENDIF
+
             WLVACi(I) = WLVAC
             CENTERi(I) = CENTER
             CONCENi(I) = CONCEN
@@ -465,6 +477,12 @@ module readfort
             ! DO J=1,5
             !   REFi(J,I) = SLABEL(J:J)
             ! END DO
+
+
+            IF(WL.GT.517.449.AND.WL.GT.517.450)THEN
+                WRITE(6,*)'D',LINDAT8,LINDAT4
+            ENDIF
+
 
             REFx(I) = REF
             NBLOi(I) = NBLO
@@ -491,10 +509,15 @@ module readfort
             ISOSHIFTi(I) = ISOSHIFT
             EXTRA3i(I) = EXTRA3
 
+
+            IF(WL.GT.517.449.AND.WL.GT.517.450)THEN
+                WRITE(6,*)'E',LINDAT8,LINDAT4
+            ENDIF
+
+
             ! IF(I.LE.10) WRITE(6,'(A8,A2)')LABELx(:,I)
             I = I + 1
             ! WRITE(6,*) 'A', OTHER1x(1,I), OTHER1x(2,I)
-            WRITE(6,*) 'B', LINDAT8,LINDAT4
 
         END DO
 100     CONTINUE
